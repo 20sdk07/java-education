25914b2481fe8b70e79530d2b71f664c274b8f16
Sadik Gun <sadikgun276@gmail.com>
2025-02-24 15:17:07 +0300

    Inheritance BTK

---
+++ src/EmployeeManagaer.java
@@ -0,0 +1,6 @@
+public class EmployeeManagaer extends PersonManager {
+
+    public void BestEmployee(){
+          System.out.println("Ayın elemanı getirildi.");
+    }
+}
---
+++ src/PersonManager.java
@@ -0,0 +1,9 @@
+public class PersonManager {
+    public void add(){
+        System.out.println("added");
+    }
+    public void list(){
+        System.out.println("listed");
+    }
+
+}
