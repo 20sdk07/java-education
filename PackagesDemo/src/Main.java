2fa32efbb0f7a09368a9dc8119c68e2c7b1e1c82
Sadik Gun <sadikgun276@gmail.com>
2025-03-05 11:20:27 +0300

    Arraylist, collections

--- PackagesDemo/src/Main.java
+++ PackagesDemo/src/Main.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         
-        Scanner scanner = new Scanner(System.in);
+Scanner scanner = new Scanner(System.in); 
+scanner.useDelimiter("\\Z");
         System.out.println("Enter your name: ");
         String name = scanner.nextLine();
         System.out.println("Hello " + name); // prints "Hello <name>"
