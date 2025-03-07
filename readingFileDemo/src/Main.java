1abe6d29cefa073572389dfbe9fc2b72f5de5c00
Sadik Gun <sadikgun276@gmail.com>
2025-03-07 17:31:51 +0300

    Prove my exception handling for file reading

--- readingFileDemo/src/Main.java
+++ readingFileDemo/src/Main.java
@@ -7,14 +7,23 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         BufferedReader reader =null;
+        int total = 0;
         try {
-        reader = new BufferedReader(new FileReader("C:\\javademos\\readingFileDemo\\src\\test.txt."));
+            reader = new BufferedReader(new FileReader("C:\\javademos\\readingFileDemo\\src\\test.txt."));
+            String line = null;
+            while((line = reader.readLine()) != null){
+                total += Integer.valueOf(line);
+        }    
+        System.out.println("Total: " + total);
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
-        }finally {
+        }catch (IOException e) {
+            System.out.println("File not found");
+        }
+        finally {
             try {
                 reader.close();
-            } catch (IOException e) {
+            } catch ( IOException e) {
                 System.out.println("File not found");
             }
         }
