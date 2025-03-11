0cbf8483fa200449f6947a8426cf304777e2aa57
Sadik Gun <sadikgun276@gmail.com>
2025-03-11 14:33:17 +0300

    work the file operations

---
+++ WorkingWithFiles/src/Main.java
@@ -0,0 +1,62 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        createFile();
+        getFileInfo();
+        readFile();
+        
+    }
+    
+    public static void createFile() {
+        File file = new File("C:\\javademos\\files\\students.txt");
+        try {
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+            // Append data to the file
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
+            writer.newLine();
+            writer.write("Ali");
+            writer.newLine();
+            writer.write("ammağğğn\n");
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+        }
+    }
+
+    public static void readFile() {
+        File file = new File("C:\\javademos\\files\\students.txt");
+        try {
+            Scanner reader = new Scanner(file);
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                System.out.println(line);
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+        }
+    }
+    
+    public static void getFileInfo() {
+        File file = new File("C:\\javademos\\files\\students.txt");
+        if (file.exists()) {
+            System.out.println("File name: " + file.getName());
+            System.out.println("Absolute path: " + file.getAbsolutePath());
+            System.out.println("Writeable: " + file.canWrite());
+            System.out.println("Readable: " + file.canRead());
+            System.out.println("File size in bytes: " + file.length());
+        } else {
+            System.out.println("The file does not exist.");
+        }
+    }
+}
